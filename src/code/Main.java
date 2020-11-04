00ed686394336b5033e05325197761c1676ffd0a
Ismael Quirós <ismaeljesusquiros@gmail.com>
2020-11-04 10:18:19 +0100

    v1.0

---
+++ src/code/Main.java
@@ -0,0 +1,27 @@
+package code;
+
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+
+	public static void main(String[] args) throws InterruptedException {
+		Almacen almacen = new Almacen();
+		Thread[] consultar = new Thread[3];
+		for(int i = 0; i < 3; i++) {
+			consultar[i] = new Thread(new Consultar(almacen, i+1), "Consultor " + i+1);
+		}
+		for(int i = 0; i < 3; i++) {
+			consultar[i].start();
+		}
+		Thread añadir = new Thread(new Añadir(almacen), "Añadir");
+		añadir.start();
+		
+		TimeUnit.SECONDS.sleep(10);
+		
+		for(int i = 0; i < 3; i++) {
+			consultar[i].interrupt();
+		}
+		añadir.interrupt();
+	}
+
+}
